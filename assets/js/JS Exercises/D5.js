863dd7e0290ebd46047af9238d46e59ef0db5486
AlessioLosi <alessiolosi93@icloud.com>
2024-06-21 16:42:32 +0200

    progetto settimana 2

--- assets/js/JS Exercises/D5.js
+++ assets/js/JS Exercises/D5.js
@@ -11,18 +11,34 @@ REGOLE
     Dato il seguente array, scrivi del codice per stampare ogni elemento dell'array in console.
 */
 const pets = ['dog', 'cat', 'hamster', 'redfish']
+for (let pets1 = 0; pets1 < pets.length; pets1++){
+  console.log(pets[pets1])
+}
+
+
 
 /* ESERCIZIO 2
     Scrivi del codice per ordinare alfabeticamente gli elementi dell'array "pets".
 */
+pets.sort();
+console.log(pets);
 
 /* ESERCIZIO 3
     Scrivi del codice per stampare nuovamente in console gli elementi dell'array "pets", questa volta in ordine invertito.
 */
+pets.reverse();
+console.log(pets)
 
 /* ESERCIZIO 4
     Scrivi del codice per spostare il primo elemento dall'array "pets" in ultima posizione.
 */
+let pets0= pets[0]
+pets.shift()
+
+pets.push(pets0)
+console.log(pets)
+
+
 
 /* ESERCIZIO 5
     Dato il seguente array di oggetti, scrivi del codice per aggiungere ad ognuno di essi una proprietà "licensePlate" con valore a tua scelta.
@@ -47,21 +63,43 @@ const cars = [
     trims: ['life', 'style', 'r-line'],
   },
 ]
-
+cars[0].licensePlate= 'AB145FG'
+cars[1].licensePlate= 'TS130SN'
+cars[2].licensePlate= 'RE689PT'
+console.log(cars)
 /* ESERCIZIO 6
     Scrivi del codice per aggiungere un nuovo oggetto in ultima posizione nell'array "cars", rispettando la struttura degli altri elementi.
     Successivamente, rimuovi l'ultimo elemento della proprietà "trims" da ogni auto.
 */
+cars.push({
+    brand: 'Ferrari',
+    licensePlate:'FR009BT',
+    model:'SF90',
+    color:'purle',
+    trims:['life', 'sport',]
+    
+  })
+console.log(cars)
+
 
 /* ESERCIZIO 7
     Scrivi del codice per salvare il primo elemento della proprietà "trims" di ogni auto nel nuovo array "justTrims", sotto definito.
 */
-const justTrims = []
+const justTrims =[]
 
 /* ESERCIZIO 8
     Cicla l'array "cars" e costruisci un if/else statament per mostrare due diversi messaggi in console. Se la prima lettera della proprietà
     "color" ha valore "b", mostra in console "Fizz". Altrimenti, mostra in console "Buzz".
 */
+function colori(cars){
+  if(cars.color.charAt(0)=='b'){
+    return 'Fizz'
+  }
+  else{ 
+    return 'Buzz'
+  }
+}
+console.log(colori(cars))
 
 /* ESERCIZIO 9
     Utilizza un ciclo while per stampare in console i valori del seguente array numerico fino al raggiungimento del numero 32.
@@ -75,4 +113,76 @@ const numericArray = [
     dell'alfabeto italiano.
     es. [f, b, e] --> [6, 2, 5]
 */
-const charactersArray = ['g', 'n', 'u', 'z', 'd']
+const charactersArray = ['g', 'n', 'u', 'z', 'd','a', 'f']
+const charactersArray2=[]
+charactersArray.forEach(char => {
+  let numeroCharacter;
+  switch (char) {
+    case 'a':
+      numeroCharacter = 1;
+      break;
+    case 'b':
+      numeroCharacter = 2;
+      break;
+    case 'c':
+      numeroCharacter = 3;
+      break;
+    case 'd':
+      numeroCharacter = 4;
+      break;
+    case 'e':
+      numeroCharacter = 5;
+      break;
+    case 'f':
+      numeroCharacter = 6;
+      break;
+    case 'g':
+      numeroCharacter = 7;
+      break;
+    case 'h':
+      numeroCharacter = 8;
+      break;
+    case 'i':
+      numeroCharacter = 9;
+      break;
+       case 'l':
+      numeroCharacter = 10;
+      break;
+    case 'm':
+      numeroCharacter = 11;
+      break;
+    case 'n':
+      numeroCharacter = 12;
+      break;
+    case 'o':
+      numeroCharacter = 13;
+      break;
+    case 'p':
+      numeroCharacter = 14;
+      break;
+    case 'q':
+      numeroCharacter = 15;
+      break;
+    case 'r':
+      numeroCharacter = 16;
+      break;
+    case 's':
+      numeroCharacter = 17;
+      break;
+    case 't':
+      numeroCharacter = 18;
+      break;
+    case 'u':
+      numeroCharacter = 19;
+      break;
+    case 'v':
+      numeroCharacter = 20;
+      break;
+    case 'z':
+      numeroCharacter = 21;
+      break;
+  }
+  charactersArray2.push(numeroCharacter);
+});
+
+console.log(charactersArray2);
